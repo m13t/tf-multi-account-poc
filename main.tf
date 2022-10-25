08e8653d2db12fbc0ea1f0cc4055e4afb82f2617
Lewis Marsden-Lambert <lewis.lambert@zserve.co.uk>
2022-10-25 15:26:12 +0100

    added VPC

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "vpc" {
+  // Address range for VPC
+  cidr_block = "10.0.0.0/16"
+
+  // We love DNS
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  // Inject account tags
+  tags = merge(module.account.tag, {
+    // Format name tag using account format
+    Name = format(module.account.tag_name.format, "VPC")
+  })
+}
