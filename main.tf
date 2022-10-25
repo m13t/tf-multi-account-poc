110590e0bf724785d7c8e9514c886fc9ea709328
Lewis Marsden-Lambert <lewis.lambert@zserve.co.uk>
2022-10-25 16:59:03 +0100

    added ability to specify additional CIDR ranges

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
+locals {
+  // Get the first CIDR
+  vpc_primary_cidr = element(module.account.env.network.cidr_blocks, 0)
+
+  // Get any additional CIDRs
+  vpc_secondary_cidrs = toset([
+    for block in module.account.env.network.cidr_blocks :
+    block if block != local.vpc_primary_cidr
+  ])
+}
+
+// Create VPC for our environment
 resource "aws_vpc" "vpc" {
   // Address range for VPC
-  cidr_block = "10.0.0.0/16"
+  cidr_block = local.vpc_primary_cidr
 
   // We love DNS
   enable_dns_support   = true
@@ -11,4 +23,12 @@ resource "aws_vpc" "vpc" {
     // Format name tag using account format
     Name = format(module.account.tag_name.format, "VPC")
   })
+}
+
+// Add any additional CIDR ranges
+resource "aws_vpc_ipv4_cidr_block_association" "additional" {
+  for_each = local.vpc_secondary_cidrs
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = each.key
 }
