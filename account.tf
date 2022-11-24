59399d5f150edfb605185c3ac85526c1b355fd5c
Lewis Marsden-Lambert <lewis.lambert@zserve.co.uk>
2022-11-24 10:14:09 +0000

    Bumped to account v0.3.0-rc.2

--- account.tf
+++ account.tf
@@ -1,6 +1,6 @@
 module "account" {
   source  = "terraform-framework/account/aws"
-  version = "0.3.0-rc.1"
+  version = "0.3.0-rc.2"
 
   // Config files are located in the accounts directory
   local_config_path = "accounts"
@@ -9,4 +9,7 @@ module "account" {
   // placeholder which will be extracted and added as a
   // tag
   local_config_subpath = "{partner}"
+
+  // Track the git org and repo
+  track_git = true
 }
